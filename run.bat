738709b753409330a84481b51229643be041d408
Nikhil Kulkarni <kulkarni.nikhil@gmail.com>
2016-06-24 13:51:14 +0530

    corrected bug in date-time variable
    Earlier, only the mm-dd was getting captured; thus if the service is run multiple times a day, the previous file of the day would get overwritten. Updated the datetimef variable to capture time also.

--- run.bat
+++ run.bat
@@ -1,6 +1,21 @@
 @echo off
-set Date=%date:~10,4%%date:~4,2%%date:~7,2%
-move $Image Galery$"\new-download\*.jpg $Image Galery$\random%Date%.jpg
+
+set hour=%time:~0,2%
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+set min=%time:~3,2%
+if "%min:~0,1%" == " " set min=0%min:~1,1%
+set secs=%time:~6,2%
+if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
+
+set month=%date:~4,2%
+if "%month:~0,1%" == " " set month=0%month:~1,1%
+set day=%date:~0,2%
+if "%day:~0,1%" == " " set day=0%day:~1,1%
+set year = %date:~-4%
+
+set datetimef=%year%_%month%%day%_%hour%%min%%secs%
+
+move $Image Galery$"\new-download\*.jpg $Image Galery$\random%datetimef%.jpg
 
 wget --trust-server-names http://source.unsplash.com/random
 
